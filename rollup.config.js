57391a73ea82aa6120da82b4dae85e476ca3b80f
yurayarosh <yura12323@gmail.com>
2019-04-28 12:06:35 +0300

    upd readme, rollup.config, remove dependency

--- rollup.config.js
+++ rollup.config.js
@@ -3,18 +3,13 @@ import babel from "rollup-plugin-babel";
 import commonjs from 'rollup-plugin-commonjs';
 
 export default {
-	entry: "src/splitter.js",
+	entry: "src/index.js",
 	format: "cjs",
 	plugins: [
 		resolve(),
 		babel({
 			exclude: "node_modules/**" // only transpile our source code
-		}),
-        commonjs({
-          namedExports: {
-            'node_modules/html-parse-stringify/index.js': [ 'HTML' ]
-          }
-        })
+		})
 	],
 	dest: "index.js" // equivalent to --output
 };
